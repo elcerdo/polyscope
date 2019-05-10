3229df72ab51db34c85ee90a26699f11483ef2fd
Pierre Gueth <pierre.gueth@allegorithmic.com>
2019-05-10 12:23:32 +0200

    build pyramid

--- src/distance-map/main.cpp
+++ src/distance-map/main.cpp
@@ -1,11 +1,52 @@
 #include "polyscope/polyscope.h"
 
+#include "geometrycentral/geometry.h"
+#include "geometrycentral/halfedge_mesh.h"
+#include "geometrycentral/polygon_soup_mesh.h"
+
+void buildPyramid() {
+    const std::vector<Vector3> positions = {
+        {-1,-1, 0},
+        {1, -1, 0},
+        {1, 1, 0},
+        {-1, 1, 0},
+        {0, 0, 1},
+    };
+
+    const std::vector<std::vector<std::size_t>> polys = {
+        {3, 2, 1, 0},
+        {0, 1, 4},
+        {1, 2, 4},
+        {2, 3, 4},
+        {3, 0, 4},
+    };
+
+    PolygonSoupMesh soup(polys, positions);
+    soup.triangulate();
+
+    Geometry<Euclidean>* geom = nullptr;
+    HalfedgeMesh* mesh = new HalfedgeMesh(soup, geom);
+
+    polyscope::registerSurfaceMesh("pyramid", geom);
+
+    VertexData<double> vertex_scalars(mesh);
+    for (const VertexPtr& vertex : mesh->vertices())
+        vertex_scalars[vertex] = geom->position(vertex).z;
+
+    polyscope::getSurfaceMesh("pyramid")->addQuantity("vertex_scalar", vertex_scalars);
+
+    delete geom;
+    delete mesh;
+}
+
 int main(int argc, char** argv) {
-		polyscope::options::autocenterStructures = true;
+    polyscope::options::autocenterStructures = true;
 
-		polyscope::init();
+    polyscope::init();
 
-		polyscope::show();
+    buildPyramid();
 
-		return 0;
+    polyscope::show();
+
+    return 0;
 }
