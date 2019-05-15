88c06c625eafd7c883cc6fea1af5094a367ae513
Pierre Gueth <pierre.gueth@gmail.com>
2019-05-16 01:26:16 +0200

    smoothest field and ribbon display

--- src/distance-map/main.cpp
+++ src/distance-map/main.cpp
@@ -3,23 +3,42 @@
 #include "geometrycentral/geometry.h"
 #include "geometrycentral/halfedge_mesh.h"
 #include "geometrycentral/polygon_soup_mesh.h"
+#include "geometrycentral/direction_fields.h"
+
+#include <memory>
+#include <random>
 
 void buildPyramid() {
-    const std::vector<Vector3> positions = {
-        {-1,-1, 0},
-        {1, -1, 0},
-        {1, 1, 0},
-        {-1, 1, 0},
-        {0, 0, 1},
-    };
+    std::default_random_engine rng;
 
-    const std::vector<std::vector<std::size_t>> polys = {
-        {3, 2, 1, 0},
-        {0, 1, 4},
-        {1, 2, 4},
-        {2, 3, 4},
-        {3, 0, 4},
-    };
+    std::vector<Vector3> positions;
+    std::vector<std::vector<size_t>> polys;
+    {
+        const size_t nn = 60;
+        const size_t mm = 45;
+
+        positions.reserve(nn * mm);
+        for (size_t ll=0; ll<mm; ll++)
+            for (size_t kk=0; kk<nn; kk++)
+            {
+                const double theta = 2*M_PI*kk/static_cast<double>(nn);
+                const double zz = 2*ll/static_cast<double>(mm) - 1;
+                positions.emplace_back(Vector3{cos(theta), sin(theta), zz});
+            }
+
+        std::bernoulli_distribution dist(.1);
+
+        polys.reserve((mm-1)*nn);
+        for (size_t ll=0; ll<mm-1; ll++)
+            for (size_t kk=0; kk<nn; kk++)
+            {
+                const size_t aa = ll*nn+kk;
+                const size_t bb = (ll+1)*nn+kk;
+                const size_t cc = (ll+1)*nn+(kk+1)%nn;
+                const size_t dd = ll*nn+(kk+1)%nn;
+                polys.emplace_back((ll + kk) % 2 == dist(rng) ? std::vector<size_t>{aa, bb, cc, dd} : std::vector<size_t>{bb, cc, dd, aa});
+            }
+    }
 
     PolygonSoupMesh soup(polys, positions);
     soup.triangulate();
@@ -29,14 +48,27 @@ void buildPyramid() {
 
     polyscope::registerSurfaceMesh("pyramid", geom);
 
-    VertexData<double> vertex_scalars(mesh);
-    for (const VertexPtr& vertex : mesh->vertices())
-        vertex_scalars[vertex] = geom->position(vertex).z;
+    {
+        polyscope::SurfaceMesh* surface = polyscope::getSurfaceMesh("pyramid");
+        std::normal_distribution<double> dist();
+        VertexData<double> vertex_scalars(mesh);
+        for (const VertexPtr& vertex : mesh->vertices())
+            vertex_scalars[vertex] = geom->position(vertex).z;
 
-    polyscope::getSurfaceMesh("pyramid")->addQuantity("vertex_scalar", vertex_scalars);
+        surface->addQuantity("vertex_scalar", vertex_scalars);
 
-    delete geom;
-    delete mesh;
+        VertexData<Complex> vertex_direction = computeSmoothestVertexDirectionField(surface->originalGeometry);
+        surface->addVectorQuantity("vertex_direction", vertex_direction);
+
+        FaceData<Complex> face_direction = computeSmoothestFaceDirectionField(surface->originalGeometry);
+        surface->addVectorQuantity("face_direction", face_direction);
+
+        delete geom;
+        delete mesh;
+    }
+}
+
+void solveSmoothField() {
 }
 
 int main(int argc, char** argv) {
@@ -45,6 +77,7 @@ int main(int argc, char** argv) {
     polyscope::init();
 
     buildPyramid();
+    solveSmoothField();
 
     polyscope::show();
 
